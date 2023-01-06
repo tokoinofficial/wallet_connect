9b74a6f99549b5acf239fe3045b676ec187f3b77
Binh Nguyen <>
2023-01-06 16:20:02 +0700

    Convert java to kotlin

---
+++ wallet_connect_android/android/src/main/kotlin/io/tokoin/wallet/connect/MyService.kt
@@ -0,0 +1,42 @@
+package io.tokoin.wallet.connect
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import androidx.annotation.Nullable
+import java.util.ArrayList
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.view.FlutterCallbackInformation
+import io.flutter.view.FlutterMain
+import io.flutter.view.FlutterNativeView
+import io.flutter.view.FlutterRunArguments
+import io.tokoin.wallet.connect.WalletConnectAndroidPlugin.Companion.CALLBACK_DISPATCHER_HANDLE_KEY
+import io.tokoin.wallet.connect.WalletConnectAndroidPlugin.Companion.CALLBACK_HANDLE_KEY
+
+class MyService : Service() {
+    @Override
+    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+        val callbackDispatcherHandle: Long = intent.getLongExtra(CALLBACK_DISPATCHER_HANDLE_KEY, 0)
+        val flutterCallbackInformation: FlutterCallbackInformation =
+            FlutterCallbackInformation.lookupCallbackInformation(callbackDispatcherHandle)
+        val flutterRunArguments = FlutterRunArguments()
+        flutterRunArguments.bundlePath = FlutterMain.findAppBundlePath()
+        flutterRunArguments.entrypoint = flutterCallbackInformation.callbackName
+        flutterRunArguments.libraryPath = flutterCallbackInformation.callbackLibraryPath
+        val backgroundFlutterView = FlutterNativeView(this, true)
+        backgroundFlutterView.runFromBundle(flutterRunArguments)
+        val mBackgroundChannel = MethodChannel(backgroundFlutterView, "background_channel")
+        val callbackHandle: Long = intent.getLongExtra(CALLBACK_HANDLE_KEY, 0)
+        val l: ArrayList<Object> = ArrayList<Object>()
+        l.add(callbackHandle)
+        l.add("Hello, I am transferred from java to dart world")
+        mBackgroundChannel.invokeMethod("", l)
+        return START_STICKY
+    }
+
+    @Nullable
+    @Override
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+}
---
+++ wallet_connect_android/android/src/main/kotlin/io/tokoin/wallet/connect/WalletConnectAndroidPlugin.kt
@@ -0,0 +1,57 @@
+package io.tokoin.wallet.connect
+
+import android.content.Context
+import android.content.Intent
+import androidx.annotation.NonNull
+import java.util.ArrayList
+import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+import io.flutter.plugin.common.MethodChannel.Result
+
+/**
+ * WalletConnectAndroidPlugin
+ */
+class WalletConnectAndroidPlugin : FlutterPlugin, MethodCallHandler {
+  private var channel: MethodChannel? = null
+  private var mContext: Context? = null
+  @Override
+  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+    channel = MethodChannel(flutterPluginBinding.getBinaryMessenger(), "wallet_connect_android")
+    channel!!.setMethodCallHandler(this)
+    mContext = flutterPluginBinding.getApplicationContext()
+  }
+
+  private var mCallbackDispatcherHandle: Long = 0
+  @Override
+  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+    if (call.method.equals("getPlatformName")) {
+      result.success("Android")
+    } else if (call.method.equals("initializeForBackground")) {
+      val args: ArrayList = call.arguments()
+      val callBackHandle = args.get(0) as Long
+      mCallbackDispatcherHandle = callBackHandle
+      result.success(null)
+      return
+    } else if (call.method.equals("callBackgroundService")) {
+      val args: ArrayList = call.arguments()
+      val callbackHandle = args.get(0) as Long
+      val i = Intent(mContext, io.tokoin.wallet.connect.MyService::class)
+      i.putExtra(CALLBACK_HANDLE_KEY, callbackHandle)
+      i.putExtra(CALLBACK_DISPATCHER_HANDLE_KEY, mCallbackDispatcherHandle)
+      mContext?.startService(i)
+      result.success(null)
+    } else result.notImplemented()
+  }
+
+  @Override
+  override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+    this.channel?.setMethodCallHandler(null)
+  }
+
+  companion object {
+    const val CALLBACK_HANDLE_KEY = "callback_handle_key"
+    const val CALLBACK_DISPATCHER_HANDLE_KEY = "callback_dispatcher_handle_key"
+  }
+}
